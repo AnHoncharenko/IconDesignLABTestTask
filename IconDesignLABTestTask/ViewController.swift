e781020027a7721330b683cd4b732efd510a392f
Anton Honcharenko <honcharenko.ay@gbksoft.com>
2023-05-09 11:00:03 +0300

    Initial Commit

---
+++ IconDesignLABTestTask/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IconDesignLABTestTask
+//
+//  Created by Anton Honcharenko on 09.05.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
